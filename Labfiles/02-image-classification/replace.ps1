b1ebba69a09866085d85df8500c24deda5e7d375
Pablo B. Martinez Pedroso <pmartinezpedroso@gmail.com>
2024-08-30 15:48:53 -0300

    Replace with valid storage account

--- Labfiles/02-image-classification/replace.ps1
+++ Labfiles/02-image-classification/replace.ps1
@@ -1,2 +1,2 @@
-$storageAcct = '<storageAccount>'
+$storageAcct = 'stgazurevisionlabs01'
 (Get-Content training-images/training_labels.json) -replace '<storageAccount>', $storageAcct | Out-File training-images/training_labels.json
